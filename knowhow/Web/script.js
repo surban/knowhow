ccc6d0ab9632489154138e0a1b7d95836eadcff1
Sebastian Urban <surban@tum.de>
2014-11-13 20:02:31 +0100

    removed refresh

--- knowhow/Web/script.js
+++ knowhow/Web/script.js
@@ -71,7 +71,7 @@ $(function () {
         if (window.applicationCache) {
             window.applicationCache.addEventListener('updateready', function (e) {
                 if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-                    window.location.reload();
+                    //window.location.reload();
                 } 
             }, false);
 
