8c0815be2d22eb2dcdeff0cfbec05643f4dedb92
Sebastian Urban <surban@tum.de>
2014-11-25 18:03:05 +0100

    cache swap

--- knowhow/Web/script.js
+++ knowhow/Web/script.js
@@ -72,6 +72,7 @@ $(function () {
         if (window.applicationCache) {
             window.applicationCache.addEventListener('updateready', function (e) {
                 if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+                    window.applicationCache.swapCache();
                     //window.location.reload();
                 } 
             }, false);
